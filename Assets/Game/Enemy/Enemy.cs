28ac8eadc266fbe77c55ce342b5ed3afb34d6900
MrSpaghettii <74720116+MrSpaghettii@users.noreply.github.com>
2023-03-18 11:45:17 -0400

    2d Niveau 1-3

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+   
+    public Transform[] waypoints;
+    private int _currentWaypointIndex = 0;
+    private float _speed = 2f;
+    private bool playerDetected = false;
+
+    private GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerDetected == false)
+        {
+            Transform wp = waypoints[_currentWaypointIndex];
+            if (Vector3.Distance(transform.position, wp.position) < 0.01f)
+            {
+                _currentWaypointIndex = (_currentWaypointIndex + 1) % waypoints.Length;
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    wp.position,
+                    _speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, _speed * Time.deltaTime);
+
+
+            Vector2 positionA = Player.transform.position;
+            Vector2 positionB = this.transform.position;
+
+            float distance = Vector2.Distance(Player.transform.position, this.transform.position);
+
+            if (Vector2.Distance(positionA, positionB) > 5)
+            {
+                playerDetected = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        if (otherObj.tag == "Player")
+        {
+            playerDetected = true;
+        }
     }
 }
